6dbbb37e78aa84396e84d63d47abeb1354d1afa2
Neil Farrington <accounts@apache-portal.com>
2015-07-27 13:55:03 -0700

    Update XmlServiceProvider.php

--- src/Vatsim/Xml/XmlServiceProvider.php
+++ src/Vatsim/Xml/XmlServiceProvider.php
@@ -12,16 +12,6 @@ class XmlServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('vatsim/xml');
-	}
-
-	/**
 	 * Register the service provider.
 	 *
 	 * @return void
